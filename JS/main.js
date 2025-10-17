776babde9a0b150505dd2bf8593c5a88dccc02b3
U-sif7elmii <helmyyousef90@gmail.com>
2025-10-17 14:38:27 +0300

    No Code Just Edit Files

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+// names.forEach( (s)=>{
+//     document.getElementById("studentCard").innerHTML += `<div class="card">
+//     <h2>${s.name}</h2>
+//     <p>${s.age}</p>
+//     <p>${s.id}</p>
+//    </div>`;
+// }
+// )
